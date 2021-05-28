4e712c68aaf415a4ae1ece4f90e784cff860df20
jm-nunez <84870508+jm-nunez@users.noreply.github.com>
2021-05-27 20:33:16 -0400

    si

--- src/components/features/Logo/index.js
+++ src/components/features/Logo/index.js
@@ -6,7 +6,7 @@ const Logo = () => (
     <Link to={HOME.link} className="font-bold text-white text-2xl ">
         <span className="flex flex-col bg-brand h-10 rounded-full justify-center items-center text-center cursor-pointer
         ">
-            <img _ngcontent-cptz-web-c64="" src="../../../assets/logo/logoSvg.svg" alt="logo" class="no-border pointer" tabindex="0"></img>
+            <img _ngcontent-cptz-web-c64="" src="https://www.capitalizarme.com/assets/images/logoWhite.png" alt="logo" class="no-border pointer" tabindex="0"></img>
         </span>
     </Link>)
 
