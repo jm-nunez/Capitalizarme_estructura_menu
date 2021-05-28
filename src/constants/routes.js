5ce261b9b6cf59b0b26227719c0f3a814d8ccdd0
jm-nunez <84870508+jm-nunez@users.noreply.github.com>
2021-05-27 20:24:08 -0400

    logo

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 export const HOME = { link: "/" };
-export const FEATURES = { name: "catalogo", link: "/features" };
+export const FEATURES = { name: "Catalogo", link: "/features" };
 export const PRODUCTS = { name: "Productos", link: "/products" };
 export const CUSTOMERS = { name: "Clientes", link: "/customers" };
 export const SALES = { name: "Venta", link: "/sales" };
-export const LOGIN = { name: "Registro", link: "/login" };
+export const LOGIN = { name: "Ingresa", link: "/login" };
