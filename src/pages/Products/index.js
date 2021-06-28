0a9afcec9cad34a7668d7f14d5a8f2c81ac02402
jm-nunez <84870508+jm-nunez@users.noreply.github.com>
2021-06-27 20:17:21 -0400

    vista list

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,11 +1,19 @@
-import React from "react"
+import React, { Fragment } from "react"
 import Card from "../../components/library/Card"
+import Submenu from "../../components/features/submenu_filter"
 
 
 const Products = () => (
-    <div className="mt-10 flex flex-col items-center pb-24">
-        <Card />
-       
-    </div>
+    <frameElement>
+    <Submenu/>
+    <div class="container mx-auto">
+            <p class="text-3xl text-gray-900">Resultados para ti:</p>
+            <p class="text-gray-700">745 Proyectos </p>
+            <div className=" flex flex-col items-center pb-24">
+            <Card />
+
+            </div>
+            </div>
+            </frameElement>
 )
 export default Products
